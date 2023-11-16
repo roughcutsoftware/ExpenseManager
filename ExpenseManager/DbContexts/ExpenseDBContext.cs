b9c3210813ab7455bee1aa683528f44c0c6f5d97
Houston <houston@roughcutsoftware.com>
2023-11-16 13:30:32 -0800

    initial clone from original repo - with minor tweaks

---
+++ ExpenseManager/DbContexts/ExpenseDBContext.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.DbContexts
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
